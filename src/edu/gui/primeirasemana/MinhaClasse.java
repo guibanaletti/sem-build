9b08acb5037f39100119e1ba06f55c3f25b16ba9
guibanaletti <guilherme_b95@live.com>
2023-05-28 23:26:21 -0300

    teste

---
+++ src/edu/gui/primeirasemana/MinhaClasse.java
@@ -0,0 +1,16 @@
+package edu.gui.primeirasemana;
+
+public class MinhaClasse {
+
+    public static void main (String [] args){
+        String primeiroNome = "Taylor";
+        String segundoNome = "Swift";
+
+        String nomeCompleto = primeiroNome.concat(" ").concat(segundoNome).concat(" is the music industry.");
+
+        System.out.println(nomeCompleto);
+    }
+
+
+
+}
